3641b4eeafc7ce1aec01d180bea3356bf95f27c2
paramesh-p98 <paramesh3598@gmail.com>
2021-12-09 21:13:53 +0530

    checkout

--- src/components/CheckoutForm/Checkout/Checkout.js
+++ src/components/CheckoutForm/Checkout/Checkout.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button, Link } from '@material-ui/core';
+import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
+import { Link } from 'react-router-dom'
 import { commerce } from '../../../lib/commerce';
 
 import useStyles from './styles'
